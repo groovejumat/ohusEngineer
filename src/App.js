ff19c2be2f5f1d1f5306962e70def796abbdb71d
tkjoo <groovejumat@naver.com>
2020-08-02 12:28:23 +0900

    fix: I changed banner

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          <h1>Hello guys I am suho.</h1>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
